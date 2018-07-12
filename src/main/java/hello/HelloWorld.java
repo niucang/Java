bfc16db369900b3efa6cf70a5a2ec97d50ee3e1d
niucang <1402703522@qq.com>
2018-07-12 10:18:14 +0800

    add gradlew

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,8 +1,13 @@
 package main.java.hello;
 
+import org.joda.time.LocalTime;
+
 public class HelloWorld {
     public static void main(String[] args) {
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
+
+        LocalTime current_time = new LocalTime();
+        System.out.println(current_time);
     }
 }
---
+++ src/main/java/hello/Waiter.java
@@ -0,0 +1,5 @@
+package main.java.hello;
+
+public class Waiter {
+    public final Integer age = 23;
+}
